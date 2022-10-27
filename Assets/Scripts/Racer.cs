91f413ac72ce9a5cb6257cb20c3f1bc14615a0b4
AteszDude <ateszdude@gmail.com>
2022-10-27 15:36:41 +0200

    Cleanup Racer.cs
    RIder cleanup

--- Assets/Scripts/Racer.cs
+++ Assets/Scripts/Racer.cs
@@ -1,45 +1,32 @@
-using System;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-public class Racer : MonoBehaviour
-{
+public class Racer : MonoBehaviour {
     public GridCell Cell { get; set; }
     public Vector2Int Speed { get; set; }
 
     [SerializeField] private GameObject mesh;
-    [SerializeField] private String name;
+    [SerializeField] private string name;
 
-    private void Awake()
-    {
-        if (mesh == null)
-        {
-            mesh = gameObject;
-        }
+    private void Awake() {
+        if (mesh == null) mesh = gameObject;
     }
 
-    public override string ToString()
-    {
+    public override string ToString() {
         return name;
     }
-    
 
-    public Vector2Int GetPosition()
-    {
+
+    public Vector2Int GetPosition() {
         Assert.IsTrue(Cell != null);
 
-        if (Cell == null)
-        {
-            return new Vector2Int(Int32.MaxValue, Int32.MaxValue);
-        }
+        if (Cell == null) return new Vector2Int(int.MaxValue, int.MaxValue);
 
         return Cell.GetPosition();
     }
 
-    public void setCell(GridCell gridcell, bool updateRot)
-    {
-        if (updateRot)
-        {
+    public void setCell(GridCell gridcell, bool updateRot) {
+        if (updateRot) {
             var direction = gridcell.GetPosition() - GetPosition();
             mesh.transform.rotation = Quaternion.LookRotation(new Vector3(direction.x, direction.y, 0),
                 new Vector3(0, 0, -1));
@@ -48,8 +35,7 @@ public class Racer : MonoBehaviour
         Cell = gridcell;
     }
 
-    public void resetRotation()
-    {
+    public void resetRotation() {
         mesh.transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 }
